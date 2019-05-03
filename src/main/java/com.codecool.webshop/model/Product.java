1e0b70986b3bbb0e16ca8c92a9eb1f8d40ac7914
Uj Levente <asdlevente@gmail.com>
2019-05-03 18:26:41 +0200

    product model class implemented

---
+++ src/main/java/com.codecool.webshop/model/Product.java
@@ -0,0 +1,43 @@
+package com.codecool.webshop.model;
+
+import com.codecool.webshop.discount.Discount;
+
+public class Product {
+
+    private Discount discount;
+    private String discountType;
+    private String name;
+    private int unitPrice;
+    private int quantity = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setQuantity(int quantityInCart) {
+        this.quantity = quantityInCart;
+    }
+
+    public Product(String name, int unitPrice, Discount discount) {
+        this.discount = discount;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public Product(String name, int unitPrice) {
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getAmountOfDiscount() {
+        if (discount == null) {
+            return 0;
+        }
+
+        return discount.calculateDiscount(quantity);
+    }
+}
---
+++ src/main/java/com.codecool.webshop/repository/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.codecool.webshop.repository;
+
+public interface ProductRepository {
+}
